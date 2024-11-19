3ed33bc3d424d9490169b2e37f7d17e28b71be4a
kaireshov <160054083+kaireshov@users.noreply.github.com>
2024-11-19 08:55:26 +0500

    change-theme.js

--- change-theme.js
+++ change-theme.js
@@ -1,14 +1,23 @@
 const themeToggleBtn = document.getElementById('theme-toggle');
 const body = document.body;
 
+const savedTheme = localStorage.getItem('theme');
+if (savedTheme) {
+  body.classList.add(savedTheme);
+  themeToggleBtn.textContent = savedTheme === 'day-theme' ? 'Ночная тема' : 'Дневная тема';
+} else {
+  body.classList.add('day-theme');
+}
+
 themeToggleBtn.addEventListener('click', () => {
   if (body.classList.contains('day-theme')) {
-    body.classList.remove('day-theme');
-    body.classList.add('night-theme');
+    body.classList.replace('day-theme', 'night-theme');
     themeToggleBtn.textContent = 'Дневная тема';
+    localStorage.setItem('theme', 'night-theme');
   } else {
-    body.classList.remove('night-theme');
-    body.classList.add('day-theme');
+    body.classList.replace('night-theme', 'day-theme');
     themeToggleBtn.textContent = 'Ночная тема';
+    localStorage.setItem('theme', 'day-theme');
   }
 });
+
